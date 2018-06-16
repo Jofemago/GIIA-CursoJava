1dd6ca93d3349bf9a25ead7145cc03e6dbbc76f7
Felipe Marín González <jofemago@utp.edu.co>
2018-06-16 17:18:49 -0500

    taller sesion2

---
+++ Semana1/Session2taller1/src/session2taller1/Exercise1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session2taller1;
+
+/**
+ *
+ * @author Pipe
+ */
+public class Exercise1 {
+    
+    
+    public static void main(String[] args) {
+        
+        for(int i = 1; i <= 100; i++){
+            
+            if( i % 10 != 2  && i %10 != 5){
+                
+                System.out.println(i);
+            }
+        }
+        
+        
+    }
+}
---
+++ Semana1/Session2taller1/src/session2taller1/Exercise2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session2taller1;
+
+/**
+ *
+ * @author Pipe
+ */
+public class Exercise2 {
+    
+    
+    public static void main(String[] args) {
+        
+        //1539 2505
+        for(int i = 1539; i <= 2505; i++){
+            
+            if(i % 2  == 0){
+                
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
