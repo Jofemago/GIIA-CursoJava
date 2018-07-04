47fe237d150101e7c4640e9d5a831345dc05556e
Felipe Marín González <jofemago@utp.edu.co>
2018-07-04 16:32:05 -0500

    agregando parte del curso swing

---
+++ Semana2/CursoJAVAswing/src/com/jofemago/swingtutorial/video2/HolaMundoFrame.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jofemago.swingtutorial.video2;
+
+/**
+ *
+ * @author Pipe
+ */
+public class HolaMundoFrame {
+    
+}
